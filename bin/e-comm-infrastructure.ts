53de6d32488b16b0f065eee1fa4dfacd27bb1883
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2025-09-10 19:48:20 -0600

    [AE-6] integrated product dynamodb table to cdk (#6)

--- bin/e-comm-infrastructure.ts
+++ bin/e-comm-infrastructure.ts
@@ -3,7 +3,6 @@ import * as cdk from 'aws-cdk-lib';
 import { ControlPlaneStack } from '../lib/control-plane-stack';
 import { REGION } from '../commons/constants';
 import { CodeArtifactStack } from '../lib/code-artifact-stack';
-import { DataStorageStack } from '../lib/data-storage-stack';
 import { ApiStack } from '../lib/api-stack';
 
 const app = new cdk.App();
@@ -21,12 +20,6 @@ const controlPlaneStack = new ControlPlaneStack(app, controlPlaneStackName, {
 
 const apiStackName = 'ECommerceApiStack';
 const apiStack = new ApiStack(app, apiStackName, {
-  env: { region: REGION },
-  controlPlaneLambda: controlPlaneStack.lambdaFunction
-});
-
-const dataStorageStackName = 'ECommerceDataStorageStack';
-new DataStorageStack(app, dataStorageStackName, {
   env: { region: REGION },
   controlPlaneLambda: controlPlaneStack.lambdaFunction
 });
--- lib/control-plane-stack.ts
+++ lib/control-plane-stack.ts
@@ -3,8 +3,13 @@ import { Construct } from 'constructs';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { AttributeType, TableV2 } from 'aws-cdk-lib/aws-dynamodb';
 
-const CONTROL_PLANE_APP_NAME = 'e-commerce-control-plane';
+const APP_NAME = 'e-commerce';
+const CONTROL_PLANE_APP_NAME = `${APP_NAME}-control-plane`;
+const USER_TABLE = `${APP_NAME}-user-table-v1`;
+const ID_PARTITION_KEY = 'id';
+const PRODUCT_TABLE = `${APP_NAME}-product-table-v1`;
 const LAMBDA_NAME = `${CONTROL_PLANE_APP_NAME}-lambda`;
 const LAMBDA_HANDLER = 'ecommerce.ECommerceControlPlaneHandler';
 const LAMBDA_TIMEOUT_SECONDS = 30;
@@ -36,10 +41,20 @@ export class ControlPlaneStack extends Stack {
         `arn:aws:s3:::${BUCKET_NAME}/${BUCKET_CODE_OBJECT_KEY}`
       ],
     }));
+
+    const userTable = new TableV2(this, USER_TABLE, {
+      partitionKey: { name: ID_PARTITION_KEY, type: AttributeType.STRING },
+      tableName: USER_TABLE
+    });
+
+    const productTable = new TableV2(this, PRODUCT_TABLE, {
+      partitionKey: { name: ID_PARTITION_KEY, type: AttributeType.STRING },
+      tableName: PRODUCT_TABLE
+    });
+
+    const tables = [userTable, productTable];
+    tables.forEach(table => {
+      table.grantReadWriteData(this.lambdaFunction);
+    });
   }
 }
-
-
-
-
-
