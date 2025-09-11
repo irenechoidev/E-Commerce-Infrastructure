490ccf15aceb0de8c8d81866a51627ba8a1f0798
Irene C <pyrom@MacBook-Air.local>
2025-09-10 19:46:33 -0600

    [AE-6] integrated product dynamodb table to cdk

--- test/control-plane-stack.test.ts
+++ test/control-plane-stack.test.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
+import { Match, Template } from 'aws-cdk-lib/assertions';
 import { ControlPlaneStack } from '../lib/control-plane-stack';
 import { CodeArtifactStack } from '../lib/code-artifact-stack';
 
@@ -11,7 +11,11 @@ const controlPlaneStack = new ControlPlaneStack(app, controlPlaneStackName, {
     bucket: codeArtifactStack.bucket
 });
 
-const CONTROL_PLANE_APP_NAME = 'e-commerce-control-plane';
+const APP_NAME = 'e-commerce';
+const CONTROL_PLANE_APP_NAME = `${APP_NAME}-control-plane`;
+const USER_TABLE = `${APP_NAME}-user-table-v1`;
+const ID_PARTITION_KEY = 'id';
+const PRODUCT_TABLE = `${APP_NAME}-product-table-v1`;
 const LAMBDA_NAME = `${CONTROL_PLANE_APP_NAME}-lambda`;
 const LAMBDA_HANDLER = 'ecommerce.ECommerceControlPlaneHandler';
 const LAMBDA_TIMEOUT_SECONDS = 30;
@@ -21,7 +25,6 @@ const BUCKET_CODE_OBJECT_KEY = 'app.jar';
 test('Control Plane Resources Created', () => {
   const template = Template.fromStack(controlPlaneStack);
 
-  // Test Lambda function
   template.hasResourceProperties('AWS::Lambda::Function', {
     FunctionName: LAMBDA_NAME,
     Runtime: 'java17',
@@ -32,16 +35,50 @@ test('Control Plane Resources Created', () => {
     Timeout: LAMBDA_TIMEOUT_SECONDS
   });
 
-  // Test IAM policy attached to the Lambda
   template.hasResourceProperties('AWS::IAM::Policy', {
     PolicyDocument: {
-      Statement: [
-        {
+      Statement: Match.arrayWith([
+        Match.objectLike({
           Action: 's3:GetObject',
           Effect: 'Allow',
-          Resource: `arn:aws:s3:::${BUCKET_NAME}/${BUCKET_CODE_OBJECT_KEY}`,
-        },
-      ],
+        }),
+      ]),
     },
+  });  
+});
+
+test('Control Plane Tables Created', () => {
+  const template = Template.fromStack(controlPlaneStack);
+
+  template.hasResourceProperties('AWS::DynamoDB::GlobalTable', {
+    TableName: USER_TABLE,
+    KeySchema: [
+      {
+        AttributeName: ID_PARTITION_KEY,
+        KeyType: 'HASH'
+      }
+    ],
+    AttributeDefinitions: [
+      {
+        AttributeName: ID_PARTITION_KEY,
+        AttributeType: 'S'
+      }
+    ]
+  });
+
+  template.hasResourceProperties('AWS::DynamoDB::GlobalTable', {
+    TableName: PRODUCT_TABLE,
+    KeySchema: [
+      {
+        AttributeName: ID_PARTITION_KEY,
+        KeyType: 'HASH'
+      }
+    ],
+    AttributeDefinitions: [
+      {
+        AttributeName: ID_PARTITION_KEY,
+        AttributeType: 'S'
+      }
+    ]
   });
 });
